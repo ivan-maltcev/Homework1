10f55f899950d50bc6a658ec5e149153b983977b
ivan-maltcev <skirym2199@gmail.com>
2025-03-27 18:48:22 +0300

    Выполнены доп задачи по js

---
+++ index.js
@@ -0,0 +1,49 @@
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+let iPhoneReleaseYear = 2007;
+alert(iPhoneReleaseYear);
+
+let jsCreator = 'Brendan Eich';
+alert('Brendan Eich');
+
+let ten = 10;
+let two = 2;
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
+
+let result = 2 ** 5;
+alert(result);
+
+let nine = 9;
+let b = 2;
+const remainder = nine % b;
+alert(remainder);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user = {
+    name: 'Иван',
+    age: 25,
+    isAdmin: true
+};
+alert(`Имя: ${user.name}, Возраст: ${user.age}, Админ: ${user.isAdmin ? 'Да' : 'Нет'}`);
+
+const userName = prompt('Как вас зовут?');
+alert(`Привет, ${userName}`);
+
